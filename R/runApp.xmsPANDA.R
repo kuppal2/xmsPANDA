df851c35ac254bdad4d7ca96c5ab78d0001466e3
Karan Uppal <kuppal2@emory.edu>
2020-07-22 06:53:32 -0400

    xmsPANDAv1.1.61j

---
+++ R/runApp.xmsPANDA.R
@@ -0,0 +1,10 @@
+runApp.xmsPANDA <-
+function() {
+
+    appDir <- system.file("shinyapp", package = "xmsPANDA")
+    if (appDir == "") {
+        stop("Could not find shinyapp directory. Try re-installing `xmsPANDA`.", call. = FALSE)
+    }
+    
+    shiny::runApp(appDir, display.mode = "normal")
+}
