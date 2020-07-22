3c4f9d127a1a49d34d635ca6d7a0fdad51239eb6
Karan Uppal <kuppal2@emory.edu>
2020-07-22 08:05:20 -0400

    xmsPANDAv1.1.61l

--- R/runApp.xmsPANDA.R
+++ R/runApp.xmsPANDA.R
@@ -1,6 +1,7 @@
 runApp.xmsPANDA <-
 function() {
-
+    library(shinyBS)
+    library(shiny)
     appDir <- system.file("shinyapp", package = "xmsPANDA")
     if (appDir == "") {
         stop("Could not find shinyapp directory. Try re-installing `xmsPANDA`.", call. = FALSE)
