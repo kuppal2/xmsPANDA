922d655773bf17829fa265fd8c224a3c4a5c2542
Karan Uppal <kuppal2@emory.edu>
2020-07-22 21:13:40 -0400

    remove all files

--- R/rnorm.trunc.R
+++
@@ -1,8 +0,0 @@
-rnorm.trunc <-
-function (n, mu, sigma, lo=-Inf, hi=Inf){
-# Calculates truncated noraml
-  p.lo = pnorm (lo, mu, sigma)
-  p.hi = pnorm (hi, mu, sigma)
-  u = runif (n, p.lo, p.hi)
-  return (qnorm (u, mu, sigma))
-}
