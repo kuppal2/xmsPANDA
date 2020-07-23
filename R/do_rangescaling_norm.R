3128f4bfb56a614787160488cc08815056376db2
Karan Uppal <kuppal2@emory.edu>
2020-07-22 21:59:27 -0400

    remove all files

--- R/do_rangescaling_norm.R
+++
@@ -1,9 +0,0 @@
-do_rangescaling_norm <-
-function(data_m)
-  {
-      #range scaling
-      data_m <- apply(data_m,1,function(x){(x-min(x))/(max(x)-min(x))})
-      
-      data_m<-t(data_m)
-      return(data_m)
-  }
