614d1500ef9f4f82f8c22941437567940c459039
Karan Uppal <kuppal2@emory.edu>
2021-02-10 14:00:51 -0800

    xmsPANDAv1.2

--- inst/shinyapp/ui.R
+++ inst/shinyapp/ui.R
@@ -23,8 +23,8 @@ ui<-fluidPage(
     ),
     column(12,
         tags$div(id="form",
-          h3(tags$img(style="float:left;margin-right:15px;",src="images/xmsPANDA_log.png",height='80px',width="80px"),"xmsPANDA - R package for biomarker discovery,
-             supervised and unsupervised learning, and network analysis (v1.2)")),
+          h3(tags$img(style="float:left;margin-right:15px;",src="images/xmsPANDA_log.png",height='80px',width="80px"),"xmsPANDA (1.2) - R package for biomarker discovery,
+             supervised and unsupervised learning, and network analysis")),
         tabsetPanel(
          
             tabPanel("Introduction", introduction_page), 
@@ -37,5 +37,6 @@ ui<-fluidPage(
         )
     ),
     column(style="padding-top:0px;padding-bottom:0px;",12,tags$hr(style="margin-top:0px;margin-bottom:15px;border-top: 0.5px solid #ccccb3;")),
- column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:95%;color:black","Maintained by Chunyu Ma & Karan Uppal (",tags$a(href="mailto:kuppal2@emory.edu","kuppal2@emory.edu"),") at Emory University, Atlanta, GA, USA")))
+ column(12,  tags$div(style="margin-center",tags$footer(align="center",color="white",style="font-weight:normal;font-size:95%;color:black","Please ask questions or report any issues on the ",
+                                                        tags$a(href='https://github.com/kuppal2/xmsPANDA/issues',target="_blank","GitHub"), " page")))
 )
