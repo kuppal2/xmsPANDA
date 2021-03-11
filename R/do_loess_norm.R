73fcbf21cc3deecd46d169ddbaa22dbf881e0be4
Karan Uppal <kuppal2@emory.edu>
2021-03-11 08:50:19 -0800

    remove all files

--- R/do_loess_norm.R
+++
@@ -1,6 +0,0 @@
-do_loess_norm <-
-function(data_m)
-{
-  data_m<-normalizeCyclicLoess(data_m)
-  return(data_m)
-}
--- R/do_znormtransform_norm.R
+++
@@ -1,8 +0,0 @@
-do_znormtransform_norm <-
-function(data_m)
-{
-  #autoscaling; z-scaling
-  data_m<-scale(t(data_m))
-  data_m<-t(data_m)
-  return(data_m)
-}
