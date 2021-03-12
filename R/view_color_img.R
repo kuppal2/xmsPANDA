04e593ada77aecbf4421020045afb721d020f7a8
Karan Uppal <kuppal2@emory.edu>
2021-03-12 10:45:44 -0800

    remove all files

--- R/view_color_img.R
+++
@@ -1,8 +0,0 @@
-view_color_img <-
-function(color.palette=c("custom1","npg","jama","jco","lancet","nejm"),alpha.col=1){
-  
-  color.palette=get_hexcolors_for_palettes(color.palette[1],alpha=alpha.col)
-  
-  show_col(color.palette,ncol=1)
-  
-}
